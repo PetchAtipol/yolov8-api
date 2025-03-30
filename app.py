00a53b98f3e32b63b6c2d18d2ccc26bed828e4aa
Eclair2199 <53sdpetch@gmail.com>
2025-03-30 22:55:54 +0700

    10th

--- app.py
+++ app.py
@@ -147,4 +147,4 @@ async def detect_latest():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
